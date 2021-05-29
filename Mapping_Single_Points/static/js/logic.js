358b5815bb6bca63a189dbb941bfe98a7c425dae
Lamis <lamis.m.nabil@gmail.com>
2021-05-29 01:14:52 -0400

    Markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("everything ok");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // add a tile layer with streets to be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -15,3 +15,10 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 })
 // add greymap tile layer to the map
 streets.addTo(map);
+
+// Add a circle marker on LA California to the map
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
