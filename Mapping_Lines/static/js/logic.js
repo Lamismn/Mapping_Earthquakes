d47a891cabe90ba7dfb4f811681ef71237ad354b
Lamis <lamis.m.nabil@gmail.com>
2021-05-29 23:16:07 -0400

    GeoJSON

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,10 +2,10 @@
 console.log("everything ok");
 
 // Create map object centered around the LAX with zoom level 5
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // add a tile layer with streets to be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -16,18 +16,24 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 // add greymap tile layer to the map
 streets.addTo(map);
 
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790]
-];
-L.polyline(line, {
-  color: "red"
-}).addTo(map);
-let lines = [
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-L.polyline(lines, {
-  color: "blue"
-}).addTo(map);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
